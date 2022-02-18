1da3201851265a25e69e3b4f42e6ef01172cc8cd
Adiel Felsen <afelsen85@gmail.com>
2022-02-17 22:20:55 -0500

    removed build and devel folders from github

--- build/assignment1-BK/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adiel/catkin_ws/src/assignment1-BK
-# Build directory: /home/adiel/catkin_ws/build/assignment1-BK
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/assignment1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adiel/catkin_ws/src/assignment1
-# Build directory: /home/adiel/catkin_ws/build/assignment1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
